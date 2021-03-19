fcd003a0144c5e01877eef52cd6f733da9df516e
Aseeter03ps <angel.2899pasj@gmail.com>
2021-03-19 15:50:24 -0600

    Modificacion de scripts
    tables scripts

--- Proyecto PWCI/db/scripts.sql
+++ Proyecto PWCI/db/scripts.sql
@@ -1,2 +1,22 @@
-Ya le modifique xd
-YO TAMBIEN YA LO HICE BB UNOS BESOS O QUE
+CREATE DATABASE IF NOT EXISTS pwciDB;
+USE pwciDB;
+
+CREATE TABLE `usuario` (
+`id` int NOT NULL AUTO_INCREMENT,
+`usuario_escuela` BOOL NOT NULL,
+`nombre` CHAR(50) NOT NULL,
+`apellidos` CHAR(50) NOT NULL,
+`correo` CHAR(50) NOT NULL,
+`contra` CHAR(50) NOT NULL,
+PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `curso`();
+
+CREATE TABLE `nivel`();
+
+CREATE TABLE `video`();
+
+CREATE TABLE `comentario`();
+
+CREATE TABLE ``();
