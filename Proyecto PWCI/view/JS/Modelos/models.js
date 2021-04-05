853e954bee91de976e4c72804b75e24fd57a1c80
Pedro Ramirez <soypedro222@hotmail.com>
2021-04-05 16:10:55 -0500

    carpeta modelos js

---
+++ Proyecto PWCI/view/JS/Modelos/models.js
@@ -0,0 +1,11 @@
+var registate = function(nombre, correo, contraseña) {
+	this.id = 0;
+	this.nombre = nombre;
+	this.correo = correo;
+	this.contraseña = contraseña;
+};
+var iniciasesion = function(correo, contraseña) {
+	this.id = 0;
+	this.correo = correo;
+	this.contraseña = contraseña;
+};
